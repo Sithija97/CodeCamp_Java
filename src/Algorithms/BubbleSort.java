5c5ed00ba93f9ce2d4255e379fe05ee55aa6a93c
Sithija Shehara <nsithijashehara@gmail.com>
2020-04-17 13:00:41 +0530

    Bubble sort-learnt

--- src/Algorithms/BubbleSort.java
+++ src/Algorithms/BubbleSort.java
@@ -1,19 +1,42 @@
 package Algorithms;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class BubbleSort {
     public static void main(String[] args) {
-        int arr[] = {5,1,4,2,8};
-        int len = arr.length;
-        int temp;
-        for(int i=0;i<len;i++){
-//            for (){
-//
-//            }
+//        int arr[] = {5,1,4,2,8};
+
+
+        System.out.println("enter values for the array:");
+        Scanner arrSize = new Scanner(System.in);
+        System.out.println("enter array size:");
+        int arrsize = arrSize.nextInt();
+
+        int arr[] = new int[arrsize];
+
+        for (int y=0;y<arrsize;y++){
+            System.out.println("enter value for "+arr[y]+"th place:");
+            Scanner value = new Scanner(System.in);
+            arr[y] = value.nextInt();
         }
 
-        System.out.println(Arrays.toString(arr));
+        System.out.println("unsorted array: "+Arrays.toString(arr));
+
+        int temp;
+
+        for(int i=0; i<arrsize; i++){
+            for(int j=0; j<arrsize-i-1; j++){
+                if (arr[j]>arr[j+1]){
+                    temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+
+                }
+            }
+        }
+
+        System.out.println("sorted array: "+Arrays.toString(arr));
 
     }
 }
---
+++ src/Algorithms/BubbleSort_CTD.java
@@ -0,0 +1,15 @@
+package Algorithms;
+
+public class BubbleSort_CTD {
+    public static void GettingArray() {
+
+    }
+
+    public static void SortingArray() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

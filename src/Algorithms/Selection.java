7cb7c35f9fa4ff9bb8de4d7eb5ed98c9b618f766
Sithija Shehara <nsithijashehara@gmail.com>
2020-04-18 22:55:50 +0530

    added revind class to revind logics

--- src/Algorithms/Selection.java
+++ src/Algorithms/Selection.java
@@ -1,4 +1,28 @@
 package Algorithms;
 
+import java.util.Arrays;
+
 public class Selection {
+    public static void SelectionSort(int data[]) {
+        for(int i=0; i<data.length-1; i++){
+            int MinIndex = i;
+            for (int j=i+1;j<data.length;j++){
+                if (data[j] < data[MinIndex]) {
+                    MinIndex = i;
+                }
+            }//finding the min
+            // put min at the correct position
+            int temp = data[i];
+            data[i] = data[MinIndex];
+            data[MinIndex] = temp;
+        }//go through the array
+        System.out.println("Sorted Array in Ascending Order: ");
+        System.out.println(Arrays.toString(data));
+    }
+
+    public static void main(String[] args) {
+        int[] data = { 20, 12, 10, 15, 2 };
+        System.out.println("unsorted array: "+ Arrays.toString(data));
+        SelectionSort(data);
+    }
 }

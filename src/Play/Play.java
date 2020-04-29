e183fa464617d5790b3536dd6e34da3e549f7bd4
Sithija Shehara <nsithijashehara@gmail.com>
2020-04-29 10:27:59 +0530

    Started working on two small games

---
+++ src/Play/Play.java
@@ -0,0 +1,10 @@
+package Play;
+
+public class Play {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Cricket game");
+        for (;;){
+            System.out.println("1");
+        }
+    }
+}

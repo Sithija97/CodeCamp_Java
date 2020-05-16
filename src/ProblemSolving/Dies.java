eafcbdc8a25119ece8984402bf99f9a010efa33a
Sithija Shehara <nsithijashehara@gmail.com>
2020-05-16 19:06:55 +0530

    Random Genearator tets

---
+++ src/ProblemSolving/Dies.java
@@ -0,0 +1,12 @@
+package ProblemSolving;
+
+import java.util.Random;
+
+public class Dies {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        //int x = rand.nextInt(10);
+        System.out.println(rand.nextInt(10));
+        System.out.println(rand.nextInt(10));
+    }
+}

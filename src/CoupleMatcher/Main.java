2c96cf17ffc6a217cd277cb7c22e883ed277c887
Sithija Shehara <nsithijashehara@gmail.com>
2021-07-09 11:08:09 +0530

    Cleared the repo

--- src/CoupleMatcher/Main.java
+++
@@ -1,9 +0,0 @@
-package CoupleMatcher;
-
-public class Main {
-    public static void main(String[] args) {
-        Facts facts = new Facts();
-        facts.Data();
-        Matcher.MatchMethod();
-    }
-}
--- src/Play/Play.java
+++
@@ -1,8 +0,0 @@
-package Play;
-import java.util.Scanner;
-
-public class Play {
-    public static void main(String[] args) {
-        Cricket.StartPlaying();
-    }
-}
